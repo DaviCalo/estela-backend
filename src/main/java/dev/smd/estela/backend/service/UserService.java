902cc528d047ddb9af76d10a2788d7d2e761b64c
Davi C. Pes. <davicalo2d@gmail.com>
2025-12-19 19:15:08 -0300

    fix bug phofile

--- src/main/java/dev/smd/estela/backend/service/UserService.java
+++ src/main/java/dev/smd/estela/backend/service/UserService.java
@@ -1,44 +1,43 @@
 package dev.smd.estela.backend.service;
 
+import static dev.smd.estela.backend.config.Config.PATH_FILES_USERS;
 import dev.smd.estela.backend.dao.UserDAO;
 import dev.smd.estela.backend.dto.UpdateUserDTO;
 import dev.smd.estela.backend.model.User;
 import java.io.File;
 
 public class UserService {
-        private static final String UPLOAD_DIRECTORY = "uploads";
-        private final UserDAO userDao = new UserDAO();
 
-        public String getProfilePhotoById(Long userId, String applicationPath) {
-                String fileName = userDao.getById(userId).getFileName();
-                String uploadPath = applicationPath + UPLOAD_DIRECTORY;
-                String filePath = uploadPath + File.separator + fileName;
-                return filePath;
-        }
+    private final UserDAO userDao = new UserDAO();
 
-        public Boolean saveNewProfilePhoto(String fileName, Long userId) {
-                if (userExists(userId) == false) {
-                        return false;
-                }
-                return userDao.updateProfilePhoto(fileName, userId);
-        }
+    public String getProfilePhotoById(Long userId, String applicationPath) {
+        String fileName = userDao.getById(userId).getFileName();
+        String filePath = PATH_FILES_USERS + File.separator + fileName;
+        return filePath;
+    }
 
-        public Boolean userExists(Long userId) {
-                User user = userDao.getById(userId);
-                return (user != null);
+    public Boolean saveNewProfilePhoto(String fileName, Long userId) {
+        if (userExists(userId) == false) {
+            return false;
         }
-        
-        public Boolean updateUser(UpdateUserDTO updateUserDTO){
-                // TODO: verificar se email já existe
-                User user = new User(updateUserDTO);
-                return  userDao.update(user);
-        }
-        
-        public User getUserById(Long userId){
-                return  userDao.getById(userId);
-        }
-        
-         public boolean  deleteUser(Long userId){
-                return  userDao.deleteById(userId);
-        }
+        return userDao.updateProfilePhoto(fileName, userId);
+    }
+
+    public Boolean userExists(Long userId) {
+        User user = userDao.getById(userId);
+        return (user != null);
+    }
+
+    public Boolean updateUser(UpdateUserDTO updateUserDTO) {
+        User user = new User(updateUserDTO);
+        return userDao.update(user);
+    }
+
+    public User getUserById(Long userId) {
+        return userDao.getById(userId);
+    }
+
+    public boolean deleteUser(Long userId) {
+        return userDao.deleteById(userId);
+    }
 }
